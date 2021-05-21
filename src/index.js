bab18de9d7c45779bc1447d6efbefece3fb962ac
yait-ucess <bandai28@icloud.com>
2021-05-21 13:32:53 +0900

    デフォルト値 引数が設定されていなかった場合、undefinedと出力されてしまうのは望ましくないため、 デフォルト値を設定しておきます。

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 /**
  * 分割代入
  */
-const myProfile = {
-  name: "たかし",
-  age: 20
-};
+// const myProfile = {
+//   name: "たかし",
+//   age: 20
+// };
 
-const { name, age } = myProfile;
-const message = `名前は${name}、${age}歳です。`;
-console.log(message);
+// const { name, age } = myProfile;
+// const message = `名前は${name}、${age}歳です。`;
+// console.log(message);
+
+// const myProfile = ["たかし", 20];
+// const [name, age] = myProfile;
+// const message = `名前は${name}、${age}歳です。`
+// console.log(message);
+
+/**
+ * デフォルト値、引数など
+ */
+const sayHello = (name = "たかし") => console.log(`こんにちは、${name}さん！`);
+sayHello();
