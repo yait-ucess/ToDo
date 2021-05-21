c45c07419a10617ac9713090aabd041f757d115f
yait-ucess <bandai28@icloud.com>
2021-05-21 15:32:28 +0900

    map, 条件式 佐藤以外には"さん"を付けるプログラム

--- src/index.js
+++ src/index.js
@@ -57,10 +57,20 @@ const nameArr = ["田中", "山田", "佐藤"];
 // })
 // console.log(nameArr2);
 
-nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
 
 // const numArr = [1, 2, 3, 4, 5];
 // const newNumArr = numArr.filter((num) => {
 //   return num % 2 === 1;
 // });
 // console.log(newNumArr);
+
+const newNameArr = nameArr.map((name) => {
+  if (name === "佐藤") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+
+console.log(newNameArr);
