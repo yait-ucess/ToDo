ca3dd53ddf230d5605671e1ebde9f84971e07cdf
yait-ucess <bandai28@icloud.com>
2021-05-21 15:27:11 +0900

    要素が何番目に出力されたかを把握する方法 要素が何番目に出力されたかを把握したい場合には、第２引数を追加します。

--- src/index.js
+++ src/index.js
@@ -47,9 +47,9 @@
 /**
  * mapやfilterを使った配列の処理
  */
-// const nameArr = ["田中", "山田", "佐藤"];
+const nameArr = ["田中", "山田", "佐藤"];
 // for (let index = 0; index < nameArr.length; index++ ) {
-//   console.log(nameArr[index]);
+//   console.log(`${index + 1}番目は${nameArr[index]}です。`);
 // }
 
 // const nameArr2 = nameArr.map((name) => {
@@ -57,10 +57,10 @@
 // })
 // console.log(nameArr2);
 
-// nameArr.map((name) => console.log(name));
+nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
 
-const numArr = [1, 2, 3, 4, 5];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 1;
-});
-console.log(newNumArr);
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
