c645a5580129f288af9ab1ac8d01138594cd1b2a
yait-ucess <bandai28@icloud.com>
2021-05-21 13:15:48 +0900

    分割代入 ${myProfile.name}のように書くのが面倒くさいので、 nameを「どのオブジェクトから参照するか」を先に定義しています。

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import "./styles.css";
+/**
+ * 分割代入
+ */
+const myProfile = {
+  name: "たかし",
+  age: 20
+};
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const { name, age } = myProfile;
+const message = `名前は${name}、${age}歳です。`;
+console.log(message);
