47bf1e1bf36808ccb75853a37b4792d2036be309
yait-ucess <bandai28@icloud.com>
2021-05-23 18:39:24 +0900

    戻すを押すと未完了リストにテキストが追加される

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ const createIncompleteList = (text) => {
 
       //テキスト取得
       const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
     });
 
     //divタグの子要素に各要素を設定
