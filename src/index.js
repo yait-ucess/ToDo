9c13ee62b3c689478cbf147b9fb40a907c365106
yait-ucess <bandai28@icloud.com>
2021-05-23 16:13:36 +0900

    入力したテキストを未完了リストに追加 document.getElementById("incomplete-list").appendChild(div);

--- src/index.js
+++ src/index.js
@@ -8,12 +8,19 @@ const onClickAdd = () => {
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
+  console.log(div);
 
   //pタグ生成
   const p = document.createElement("p");
   p.className = "todo-name";
   p.innerText = inputText;
   console.log(p);
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(p);
+
+  //入力したテキストを未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
