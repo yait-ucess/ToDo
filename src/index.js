58fdc82e851bc505efb82e88525ff682d2c9364b
yait-ucess <bandai28@icloud.com>
2021-05-23 16:45:18 +0900

    完了、削除buttonにclickイベントを追加

--- src/index.js
+++ src/index.js
@@ -16,8 +16,24 @@ const onClickAdd = () => {
   p.innerText = inputText;
   console.log(p);
 
+  //button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   //divタグの子要素に各要素を設定
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //入力したテキストを未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
